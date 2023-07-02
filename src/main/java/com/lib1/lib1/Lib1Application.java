ec6b1217a2bb2d7a510011e70eabcc11f2babb1f
Kevin <namly5036@gmail.com>
2023-07-02 22:06:45 +0700

    add code

---
+++ src/main/java/com/lib1/lib1/Lib1Application.java
@@ -0,0 +1,13 @@
+package com.lib1.lib1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lib1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lib1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/lib1/lib1/Lib1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lib1.lib1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lib1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
